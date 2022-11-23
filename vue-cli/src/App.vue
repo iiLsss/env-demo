277c456fab44cbe99877ad7a89c7bbf1ae2e67c0
iiLsss <lish1993@163.com>
2022-11-23 09:59:54 +0800

    feat: add-cli

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <p>当前运行环境: {{ env }}</p>
+    <p>当前请求接口地址: {{ api }}</p>
+  </div>
+</template>
+
+<script>
+import { currentEnv, API_URL } from '@/const'
+export default {
+  name: 'App',
+  data() {
+    return {
+      env: currentEnv,
+      api: API_URL,
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli/src/const/index.js
@@ -0,0 +1,9 @@
+export const currentEnv = process.env.VUE_APP_ENV
+
+export const API_URL = process.env.VUE_APP_API_URL
+
+export const COMMON = process.env.VUE_APP_COMMON
+
+console.log('COMMON', COMMON)
+console.log('currentEnv', currentEnv)
+console.log('API_URL', API_URL)
